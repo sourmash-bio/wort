719354df806f82478afd23f647d4c5a1eaa47496
Luiz Irber <luiz.irber@gmail.com>
2018-02-21 11:52:44 -0800

    add IMG support to viewer

--- wort/blueprints/viewer/views.py
+++ wort/blueprints/viewer/views.py
@@ -5,19 +5,29 @@ from flask import Blueprint, render_template, jsonify, redirect
 viewer = Blueprint('viewer', __name__, template_folder='templates')
 
 
-@viewer.route('/view/sra/<sra_id>')
-def view_sra(sra_id):
+@viewer.route('/view/<db>/<dataset_id>')
+def view_s3(db, dataset_id):
+
+    if db not in ('sra', 'img'):
+        return 404
+
     import boto3
 
     conn = boto3.client('s3')
 
+    key = f'sigs/{dataset_id}.sig'
+    # TODO: we don't really need this, I just copied the signatures improperly
+    # to the bucket...
+    if db == 'img':
+        key = f'{dataset_id}.sig'
+
     url = conn.generate_presigned_url(
         'get_object',
         Params={
-            'Bucket': "wort-sra",
-            'Key': os.path.join('sigs', sra_id + '.sig'),
+            'Bucket': f"wort-{db}",
+            'Key': key,
             'ResponseContentType': 'application/json',
-            #               'ResponseContentEncoding': 'gzip',
+            #'ResponseContentEncoding': 'gzip',
         },
         ExpiresIn=100)
 
