92054dea4cb8633a58e30fb3b1b4cc7903734e84
Luiz Irber <luiz.irber@gmail.com>
2018-09-26 17:02:34 -0700

    fix shell

--- wortapp.py
+++ wortapp.py
@@ -2,9 +2,9 @@ from wort.app import create_app
 from wort.ext import db
 from wort.models import User, Task
 
-app = create_app()
+app = create_app().app
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Task': Task}
+    return {"db": db, "User": User, "Task": Task}
