7f4496b48ffb68862699e03c0c919ca69a371a44
Luiz Irber <luiz.irber@gmail.com>
2018-10-05 16:22:44 -0700

    Only submit a job if key doesn't exit

--- wort/blueprints/compute/views.py
+++ wort/blueprints/compute/views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, render_template, jsonify, url_for
 
 compute = Blueprint("compute", __name__, template_folder="templates")
@@ -8,7 +10,25 @@ from wort.blueprints.api.auth import token_auth
 # @compute.route("/compute/sra/<sra_id>", methods=["POST"])
 @token_auth.login_required
 def compute_sra(sra_id):
+    import boto3
+    import botocore
+
     from . import tasks
 
+    s3 = boto3.resource("s3")
+
+    key_path = os.path.join("sigs", sra_id + ".sig")
+    try:
+        s3.Object("wort-sra", key_path).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            pass  # Object does not exist, let's compute it later
+        else:
+            # Something else has gone wrong
+            raise
+    else:
+        # The key already exists
+        return jsonify({"status": "Signature already calculated"}), 202
+
     task = tasks.compute.delay(sra_id)
-    return jsonify({"task_id": task.id}), 202
+    return jsonify({"status": "Submitted", "task_id": task.id}), 202
