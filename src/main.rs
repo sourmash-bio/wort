e6612645ea90dca2b163b95849315f327ea0bdba
Luiz Irber <luiz.irber@gmail.com>
2020-06-03 15:57:15 -0700

    update rust cli

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-#[macro_use]
-extern crate clap;
-extern crate dialoguer;
-extern crate keyring;
-extern crate reqwest;
-#[macro_use]
-extern crate serde_derive;
+use std::error::Error as _;
 
-use std::error::Error;
-
-use clap::App;
-use dialoguer::{Input, PasswordInput};
+use clap::{load_yaml, App};
+use dialoguer::{Input, Password};
+use eyre::{eyre, Result};
 use reqwest::StatusCode;
+use serde::Deserialize;
 
 const BASEURL: &'static str = "https://wort.oxli.org/v1";
 const SERVICENAME: &'static str = "wort";
@@ -20,20 +14,29 @@ struct Response {
     status: String,
 }
 
-fn view(db: &str, dataset_id: &str) -> Result<(), Box<Error>> {
+fn view(db: &str, dataset_id: &str) -> Result<()> {
     let url = format!("{}/view/{}/{}", BASEURL, db, dataset_id);
-    let mut res = reqwest::get(&url)?;
+    let mut res = reqwest::blocking::get(&url)?;
     std::io::copy(&mut res, &mut std::io::stdout())?;
     Ok(())
 }
 
-fn submit(db: String, dataset_id: String, token: &str, filename: &str) -> Result<(), Box<Error>> {
-    let form = reqwest::multipart::Form::new().file("file", filename)?;
+fn compute(db: String, dataset_id: String, token: &str) -> Result<()> {
+    let url = format!("{}/compute/{}/{}", BASEURL, db, dataset_id);
+
+    let client = reqwest::blocking::Client::new();
+    let res = client.post(&url).bearer_auth(token).send()?;
+    println!("{}", res.json::<Response>()?.status);
+    Ok(())
+}
+
+fn submit(db: String, dataset_id: String, token: &str, filename: &str) -> Result<()> {
+    let form = reqwest::blocking::multipart::Form::new().file("file", filename)?;
 
     let url = format!("{}/submit/{}/{}", BASEURL, db, dataset_id);
 
-    let client = reqwest::Client::new();
-    let mut res = client
+    let client = reqwest::blocking::Client::new();
+    let res = client
         .post(&url)
         .bearer_auth(token)
         .multipart(form)
@@ -42,12 +45,12 @@ fn submit(db: String, dataset_id: String, token: &str, filename: &str) -> Result
     Ok(())
 }
 
-fn get_remote_token(username: &str, password: &str) -> Result<String, Box<Error>> {
-    let client = reqwest::Client::new();
+fn get_remote_token(username: &str, password: &str) -> Result<String> {
+    let client = reqwest::blocking::Client::new();
 
     let url = format!("{}/auth/tokens", BASEURL);
 
-    let mut resp = client
+    let resp = client
         .post(&url)
         .basic_auth(username, Some(password))
         .send()?;
@@ -62,12 +65,16 @@ fn get_remote_token(username: &str, password: &str) -> Result<String, Box<Error>
     Ok(token)
 }
 
-fn get_saved_token() -> Result<String, Box<Error>> {
+fn get_saved_token() -> Result<String> {
     let keyring = keyring::Keyring::new(&SERVICENAME, "token");
-    Ok(String::from(keyring.get_password()?))
+    Ok(String::from(
+        keyring
+            .get_password()
+            .map_err(|_| eyre!("Unable to get password"))?,
+    ))
 }
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Result<()> {
     let yml = load_yaml!("wort.yml");
     let m = App::from_yaml(yml).get_matches();
 
@@ -77,6 +84,20 @@ fn main() -> Result<(), Box<Error>> {
             view(
                 cmd.value_of("database").unwrap(),
                 cmd.value_of("dataset_id").unwrap(),
+            )
+        }
+        Some("compute") => {
+            let cmd = m.subcommand_matches("compute").unwrap();
+
+            let token = match cmd.value_of("token") {
+                Some(n) => n.into(),
+                None => get_saved_token()?,
+            };
+
+            compute(
+                cmd.value_of("database").unwrap().into(),
+                cmd.value_of("dataset_id").unwrap().into(),
+                &token,
             )
         }
         Some("submit") => {
@@ -104,17 +125,19 @@ fn main() -> Result<(), Box<Error>> {
 
             let password: String = match cmd.value_of("password") {
                 Some(n) => n.into(),
-                None => PasswordInput::new().with_prompt("Password").interact()?,
+                None => Password::new().with_prompt("Password").interact()?,
             };
 
             let token = get_remote_token(&username, &password)?;
 
             let keyring = keyring::Keyring::new(&SERVICENAME, "token");
-            keyring.set_password(&token)?;
+            keyring
+                .set_password(&token)
+                .map_err(|e| eyre!("Unable to set password: {}", e.source().unwrap()))?;
 
             Ok(())
         }
-        None => Ok(()), // TODO: should be error
-        _ => Ok(()),    // TODO: should be error
+        Some(c) => Err(eyre!("Unknown subcommand {}", c)),
+        None => Err(eyre!("Subcommand missing!")),
     }
 }
