d34457eba195c3c92d0fecbe9e4a12db3969b285
Luiz Irber <luiz.irber@gmail.com>
2020-07-05 11:47:32 -0700

    compute queues split, check size_MB

--- wortapp.py
+++ wortapp.py
@@ -1,10 +1,10 @@
 from wort.app import create_app
 from wort.ext import db
-from wort.models import Task, User
+from wort.models import Database, Dataset, User
 
 app = create_app().app
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Task": Task}
+    return {"db": db, "User": User, "Database": Database, "Dataset": Dataset}
