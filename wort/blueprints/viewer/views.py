ad5ef1ea21a5051a115ab02a0d936402a82ee73d
Luiz Irber <luizirber@users.noreply.github.com>
2020-07-22 03:54:07 +0000

    redirect to IPFS only (#33)

--- wort/blueprints/viewer/views.py
+++ wort/blueprints/viewer/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, redirect, render_template
+from flask import Blueprint, current_app, jsonify, redirect, render_template
 
 viewer = Blueprint("viewer", __name__, template_folder="templates")
 
@@ -25,3 +25,25 @@ def view_s3(public_db, dataset_id):
     url = conn.generate_presigned_url("get_object", Params=params, ExpiresIn=100)
 
     return redirect(url)
+
+# @viewer.route("/view/<db>/<dataset_id>")
+def view(public_db, dataset_id):
+
+    if public_db not in ("sra", "img"):
+        return "Database not supported", 404
+
+    dataset_info = current_app.cache.get(f"{public_db}/{dataset_id}")
+
+    if dataset_info is None:
+        # Not in cache, let's check DB
+        dataset = Dataset.query.filter_by(id=dataset_id).first()
+
+        if dataset is not None:
+            # Found a hit in DB
+            if dataset.ipfs is not None:
+                return redirect(f"https://cloudflare-ipfs.com/ipfs/{dataset.ipfs}")
+    else:
+        # Found in cache, redirect
+        return redirect(f"https://cloudflare-ipfs.com/ipfs/{dataset_info['ipfs']}")
+
+    return "Dataset not found", 404
