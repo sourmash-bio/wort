c3f156ab4212d4d0d1717bfc9fae72230a296ef2
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2023-08-03 09:17:56 -0700

    clean up

--- machine/wort-web/daily-sra-biopython.py
+++ machine/wort-web/daily-sra-biopython.py
@@ -1,13 +1,15 @@
+import sys
+import argparse
 import datetime
 from Bio import Entrez
 import pandas as pd
 
-def main():
+def main(args):
     # Get yesterday's date
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
     day_to_check = yesterday.strftime("%Y/%m/%d")
 
-    Entrez.email = 'ntpierce@gmail.com'  # Set your email address
+    Entrez.email = args.email  # Set email address
 
     # Perform the Biopython search for genomic samples
     query_dna = f'("{day_to_check}"[PDAT] : "3000"[PDAT]) AND "biomol dna"[Properties] NOT amplicon[All Fields] NOT "Metazoa"[orgn:__txid33208] NOT "Streptophyta"[orgn:__txid35493] NOT "METAGENOMIC"[Source]'
@@ -15,18 +17,17 @@ def main():
     record_dna = Entrez.read(handle_dna)
     handle_dna.close()
 
-    # Fetch the DNA sample records using Entrez.efetch
+    # Fetch the genomic sample records using Entrez.efetch
     id_list_dna = record_dna["IdList"]
     if not id_list_dna:
-        print("No DNA samples found for yesterday.")
+        print("No genomic samples found for yesterday.")
     else:
         handle_dna_records = Entrez.efetch(db='sra', id=id_list_dna, rettype='runinfo', retmode='text')
         df_dna = pd.read_csv(handle_dna_records)
 
-        # Save the DNA sample records to a CSV file
-        output_filename = "runinfo_dna.csv"
-        df_dna.to_csv(output_filename, index=False)
-        print(f"DNA samples saved to: {output_filename}")
+        # Save the genomic sample records to a CSV file
+        df_dna.to_csv(args.genomes, index=False)
+        print(f"Genomic samples saved to: {args.genomes}")
 
     # Perform the Biopython search for metagenomic samples
     query_metagenomic = f'("{day_to_check}"[PDAT] : "3000"[PDAT]) NOT amplicon[All Fields] AND "METAGENOMIC"[Source]'
@@ -43,9 +44,20 @@ def main():
         df_metagenomic = pd.read_csv(handle_metagenomic_records)
 
         # Save the metagenomic sample records to a CSV file
-        output_filename = "runinfo.csv"
-        df_metagenomic.to_csv(output_filename, index=False)
-        print(f"Metagenomic samples saved to: {output_filename}")
+        df_metagenomic.to_csv(args.metagenomes, index=False)
+        print(f"Metagenomic samples saved to: {args.genomes}")
 
-if __name__ == "__main__":
-    main()
+def cmdline(sys_args):
+    "Command line entry point w/argparse action."
+    p = argparse.ArgumentParser(description="Download runinfo for metagenomes and genomes released yesterday.")
+    p.add_argument('--metagenomes', type=str, help="output file for metagenomes")
+    p.add_argument('--genomes', type=str, help="output file for genomes")
+    p.add_argument('--email', default='admin@sourmash.bio')
+    # Parse command line arguments
+    args = p.parse_args()
+    # Download sequences for the specified accession
+    return main(args)
+
+if __name__ == '__main__':
+    returncode = cmdline(sys.argv[1:])
+    sys.exit(returncode)
--- machine/wort-web/download_daily_sra.sh
+++ machine/wort-web/download_daily_sra.sh
@@ -6,14 +6,14 @@ DAY_TO_CHECK=${1:-yesterday}
 
 day_plain=$(date -d $DAY_TO_CHECK +%Y%m%d)
 
-output=outputs/${day_plain}.csv
+output_genomes=outputs/${day_plain}.csv
 output_metagenomes=outputs_metagenomes/${day_plain}.csv
 
 conda activate biopython
 python daily-sra-biopython.py --metagenomes ${output_metagenomes} --genomes ${output_genomes}
 
-#docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output}"
-#pipenv run python submit.py ${output} | tee logs/`date +%Y%m%d_%H%M`.submitted
+docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output_genomes}"
+pipenv run python submit.py ${output_genomes} | tee logs/`date +%Y%m%d_%H%M`.submitted
 
-#docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output_metagenomes}"
-#pipenv run python submit.py ${output_metagenomes} | tee logs_metagenomes/`date +%Y%m%d_%H%M`.submitted
+docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output_metagenomes}"
+pipenv run python submit.py ${output_metagenomes} | tee logs_metagenomes/`date +%Y%m%d_%H%M`.submitted
