b30fa1eb1ab2b6626d295ea52e1d2885f6d121f1
Luiz Irber <contact+github@luizirber.org>
2023-08-16 16:42:54 -0700

    add biopython and pandas dep, run daily update on web container for now

--- machine/wort-web/download_daily_sra.sh
+++ machine/wort-web/download_daily_sra.sh
@@ -9,8 +9,7 @@ day_plain=$(date -d $DAY_TO_CHECK +%Y%m%d)
 output_genomes=outputs/${day_plain}.csv
 output_metagenomes=outputs_metagenomes/${day_plain}.csv
 
-conda activate biopython
-python daily-sra-biopython.py --metagenomes ${output_metagenomes} --genomes ${output_genomes}
+docker-compose exec -T web flask shell -c "%run -i machine/wort-web/daily-sra-biopython.py --metagenomes machine/wort-web/${output_metagenomes} --genomes machine/wort-web/${output_genomes}"
 
 docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output_genomes}"
 pipenv run python submit.py ${output_genomes} | tee logs/`date +%Y%m%d_%H%M`.submitted
