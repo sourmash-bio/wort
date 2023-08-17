f35feeb7a67075f8f630aa619e24a86370341f16
Luiz Irber <contact+github@luizirber.org>
2023-08-16 18:39:03 -0700

    fix output naming

--- machine/wort-web/download_daily_sra.sh
+++ machine/wort-web/download_daily_sra.sh
@@ -7,14 +7,14 @@ day_plain=$(date -d $DAY_TO_CHECK +%Y%m%d)
 
 output_genomes=outputs/${day_plain}.csv
 output_metagenomes=outputs_metagenomes/${day_plain}.csv
-output_metatranscriptomics=outputs_metatranscriptomics/${day_plain}.csv
+output_metatranscriptomes=outputs_metatranscriptomes/${day_plain}.csv
 
 submit_csv() {
   docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/$1"
   pipenv run python submit.py $1 | tee $2/`date +%Y%m%d_%H%M`.submitted
 }
 
-docker-compose exec -T web flask shell -c "%run -i machine/wort-web/daily-sra-biopython.py --metagenomes machine/wort-web/${output_metagenomes} --genomes machine/wort-web/${output_genomes} --metatranscriptomics machine/wort-web/${output_metatranscriptomics}"
+docker-compose exec -T web flask shell -c "%run -i machine/wort-web/daily-sra-biopython.py --metagenomes machine/wort-web/${output_metagenomes} --genomes machine/wort-web/${output_genomes} --metatranscriptomes machine/wort-web/${output_metatranscriptomes}"
 
 submit_csv ${output_genomes} logs &
 submit_csv ${output_metagenomes} logs_metagenomes &
