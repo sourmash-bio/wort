09de84a809853071ee7bb82f9594e503495244a9
Luiz Irber <contact+github@luizirber.org>
2023-08-16 18:20:35 -0700

    refactor

--- machine/wort-web/daily-sra-biopython.py
+++ machine/wort-web/daily-sra-biopython.py
@@ -5,59 +5,49 @@ from Bio import Entrez
 import pandas as pd
 
 def main(args):
-    # Get yesterday's date
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    day_to_check = yesterday.strftime("%Y/%m/%d")
-
+    day_to_check = args.start.strftime("%Y/%m/%d")
     Entrez.email = args.email  # Set email address
 
     # Perform the Biopython search for genomic samples
     query_dna = f'("{day_to_check}"[PDAT] : "3000"[PDAT]) AND "biomol dna"[Properties] NOT amplicon[All Fields] NOT "Metazoa"[orgn:__txid33208] NOT "Streptophyta"[orgn:__txid35493] NOT "METAGENOMIC"[Source]'
-    handle_dna = Entrez.esearch(db='sra', term=query_dna, retmax=1000000)
-    record_dna = Entrez.read(handle_dna)
-    handle_dna.close()
+    query_metagenomic = f'("{day_to_check}"[PDAT] : "3000"[PDAT]) NOT amplicon[All Fields] AND "METAGENOMIC"[Source]'
+    query_metatranscriptomic = f'("{day_to_check}"[PDAT] : "3000"[PDAT]) NOT amplicon[All Fields] AND "METATRANSCRIPTOMIC"[Source]'
+
+    download_entrez(query_dna, args.genomes, "genomic")
+    download_entrez(query_metagenomic, args.metagenomes, "metagenomic")
+    download_entrez(query_metatranscriptomic, args.metatranscriptomes, "metatranscriptomic")
+
+
+def download_entrez(query, output, data_type="genomic"):
+    handle = Entrez.esearch(db='sra', term=query, retmax=10000)
+    record = Entrez.read(handle)
+    handle.close()
 
     # Fetch the genomic sample records using Entrez.efetch
-    id_list_dna = record_dna["IdList"]
-    if not id_list_dna:
-        print("No genomic samples found for yesterday.")
+    id_list = record["IdList"]
+    if not id_list:
+        print(f"No {data_type} samples found for yesterday.")
     else:
-        handle_dna_records = Entrez.efetch(db='sra', id=id_list_dna, rettype='runinfo', retmode='text')
-        df_dna = pd.read_csv(handle_dna_records)
+        handle_records = Entrez.efetch(db='sra', id=id_list, rettype='runinfo', retmode='text')
+        df = pd.read_csv(handle_records)
 
         # Save the genomic sample records to a CSV file
-        df_dna.to_csv(args.genomes, index=False)
-        print(f"Genomic samples saved to: {args.genomes}")
+        df.to_csv(output, index=False)
+        print(f"{data_type} samples saved to: {output}")
 
-    # Perform the Biopython search for metagenomic samples
-    query_metagenomic = f'("{day_to_check}"[PDAT] : "3000"[PDAT]) NOT amplicon[All Fields] AND "METAGENOMIC"[Source]'
-    handle_metagenomic = Entrez.esearch(db='sra', term=query_metagenomic, retmax=1000000)
-    record_metagenomic = Entrez.read(handle_metagenomic)
-    handle_metagenomic.close()
-
-    # Fetch the metagenomic sample records using Entrez.efetch
-    id_list_metagenomic = record_metagenomic["IdList"]
-    if not id_list_metagenomic:
-        print("No metagenomic samples found for yesterday.")
-    else:
-        handle_metagenomic_records = Entrez.efetch(db='sra', id=id_list_metagenomic, rettype='runinfo', retmode='text')
-        df_metagenomic = pd.read_csv(handle_metagenomic_records)
-
-        # Save the metagenomic sample records to a CSV file
-        df_metagenomic.to_csv(args.metagenomes, index=False)
-        print(f"Metagenomic samples saved to: {args.genomes}")
-
-def cmdline(sys_args):
-    "Command line entry point w/argparse action."
-    p = argparse.ArgumentParser(description="Download runinfo for metagenomes and genomes released yesterday.")
-    p.add_argument('--metagenomes', type=str, help="output file for metagenomes")
-    p.add_argument('--genomes', type=str, help="output file for genomes")
-    p.add_argument('--email', default='admin@sourmash.bio')
-    # Parse command line arguments
-    args = p.parse_args()
-    # Download sequences for the specified accession
-    return main(args)
 
 if __name__ == '__main__':
-    returncode = cmdline(sys.argv[1:])
-    sys.exit(returncode)
+    # Get yesterday's date
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+
+    p = argparse.ArgumentParser(description="Download runinfo for metagenomes, metatranscriptomics, and genomes released yesterday.")
+    p.add_argument('--metagenomes', type=str, help="output file for metagenomes")
+    p.add_argument('--genomes', type=str, help="output file for genomes")
+    p.add_argument('--metatranscriptomes', type=str, help="output file for metatranscriptomes")
+    p.add_argument('--email', default='admin@sourmash.bio')
+    p.add_argument('--start', default=yesterday, type=datetime.date.fromisoformat)
+    # Parse command line arguments
+    args = p.parse_args()
+
+    # Download sequences for the specified accession
+    sys.exit(main(args))
--- machine/wort-web/download_daily_sra.sh
+++ machine/wort-web/download_daily_sra.sh
@@ -3,16 +3,21 @@
 set -exu
 
 DAY_TO_CHECK=${1:-yesterday}
-
 day_plain=$(date -d $DAY_TO_CHECK +%Y%m%d)
 
 output_genomes=outputs/${day_plain}.csv
 output_metagenomes=outputs_metagenomes/${day_plain}.csv
+output_metatranscriptomics=outputs_metatranscriptomics/${day_plain}.csv
 
-docker-compose exec -T web flask shell -c "%run -i machine/wort-web/daily-sra-biopython.py --metagenomes machine/wort-web/${output_metagenomes} --genomes machine/wort-web/${output_genomes}"
+submit_csv() {
+  docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/$1"
+  pipenv run python submit.py $1 | tee $2/`date +%Y%m%d_%H%M`.submitted
+}
 
-docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output_genomes}"
-pipenv run python submit.py ${output_genomes} | tee logs/`date +%Y%m%d_%H%M`.submitted
+docker-compose exec -T web flask shell -c "%run -i machine/wort-web/daily-sra-biopython.py --metagenomes machine/wort-web/${output_metagenomes} --genomes machine/wort-web/${output_genomes} --metatranscriptomics machine/wort-web/${output_metatranscriptomics}"
 
-docker-compose exec -T web flask shell -c "%run -i machine/wort-web/add_dataset_info.py machine/wort-web/${output_metagenomes}"
-pipenv run python submit.py ${output_metagenomes} | tee logs_metagenomes/`date +%Y%m%d_%H%M`.submitted
+submit_csv ${output_genomes} logs &
+submit_csv ${output_metagenomes} logs_metagenomes &
+submit_csv ${output_metatranscriptomes} logs_metatranscriptomes &
+
+wait
